1ed4c53c9e87f1e124d9bd0922e76085625dbdf3
1dmiranda <barbosamiranda418@hotmail.com>
2021-02-09 22:07:19 -0300

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React, {FC} from "react";
+import ReactDOM from "react-dom"
+
+const App: FC = () => {
+    return(
+        <div>React Webpack</div>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById("root"))
